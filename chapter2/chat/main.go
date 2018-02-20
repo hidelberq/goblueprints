58a3888f9b63861b35231fcbf7581af4dffcb8fd
Hidenori Ogawa <hide.seaweed@gmail.com>
2018-02-20 23:14:29 +0900

    add chapter2

---
+++ chapter2/chat/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+
+	"github.com/hidelbreq/goblueprints/chapter1/trace"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ =
+			template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, r)
+}
+
+func main() {
+	var addr = flag.String("addr", ":8081", "アプリケーションのアドレス")
+	flag.Parse()
+	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+	go r.run()
+	log.Println("Web サーバーを起動します。ポート: ", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal(err)
+	}
+}
