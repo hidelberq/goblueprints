63763e46f67db9c5a6922c2d0745c113bceb4bc2
Hidenori Ogawa <hide.seaweed@gmail.com>
2018-02-21 19:17:41 +0900

    add chapter3

---
+++ chapter3/chat/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"flag"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+
+	"github.com/hidelbreq/goblueprints/chapter1/trace"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.templ =
+		template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+	t.templ.Execute(w, data)
+}
+
+func main() {
+	var addr = flag.String("addr", ":8081", "アプリケーションのアドレス")
+	flag.Parse()
+	gomniauth.SetSecurityKey("")
+	gomniauth.WithProviders(
+		google.New(
+			"",
+			"",
+			"",
+		),
+	)
+	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
+	http.Handle("/room", r)
+	go r.run()
+	log.Println("Web サーバーを起動します。ポート: ", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ chapter3/chat/room.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	"github.com/hidelbreq/goblueprints/chapter1/trace"
+	"github.com/stretchr/objx"
+)
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+type room struct {
+	forward chan *message
+	join    chan *client
+	leave   chan *client
+	clients map[*client]bool
+	tracer  trace.Tracer
+}
+
+func newRoom() *room {
+	return &room{
+		forward: make(chan *message, messageBufferSize),
+		join:    make(chan *client),
+		leave:   make(chan *client),
+		clients: make(map[*client]bool),
+		tracer:  trace.Off(),
+	}
+}
+
+var upgrader = &websocket.Upgrader{
+	ReadBufferSize:  socketBufferSize,
+	WriteBufferSize: messageBufferSize,
+}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP:", err)
+		return
+	}
+
+	authCookie, err := req.Cookie("auth")
+	if err != nil {
+		log.Fatalln("クッキーの取得に失敗しました")
+		return
+	}
+
+	client := &client{
+		socket:   socket,
+		send:     make(chan *message, messageBufferSize),
+		room:     r,
+		userData: objx.MustFromBase64(authCookie.Value),
+	}
+	r.join <- client
+	defer func() { r.leave <- client }()
+	go client.write()
+	client.read()
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			r.clients[client] = true
+			r.tracer.Trace("新しいクライアントが参加しました")
+		case client := <-r.leave:
+			delete(r.clients, client)
+			close(client.send)
+			r.tracer.Trace("クライアントが退室しました")
+		case msg := <-r.forward:
+			r.tracer.Trace("メッセージを受信しました: ", msg.Message)
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					r.tracer.Trace(" -- クライアントに送信されました")
+				default:
+					delete(r.clients, client)
+					close(client.send)
+					r.tracer.Trace(" -- 送信に失敗しました。クライアントをクリーンアップします")
+				}
+			}
+		}
+	}
+}
