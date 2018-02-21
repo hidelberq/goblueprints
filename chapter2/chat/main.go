dca3f5e8e15231592968f41eb2066111d9da5031
Hidenori Ogawa <hide.seaweed@gmail.com>
2018-02-21 17:41:06 +0900

    add authorized user name

--- chapter2/chat/main.go
+++ chapter2/chat/main.go
@@ -10,6 +10,9 @@ import (
 	"sync"
 
 	"github.com/hidelbreq/goblueprints/chapter1/trace"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
 )
 
 type templateHandler struct {
@@ -21,12 +24,27 @@ type templateHandler struct {
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.templ =
 		template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
-	t.templ.Execute(w, r)
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+
+	if authCo0kie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCo0kie.Value)
+	}
+	t.templ.Execute(w, data)
 }
 
 func main() {
 	var addr = flag.String("addr", ":8081", "アプリケーションのアドレス")
 	flag.Parse()
+	gomniauth.SetSecurityKey("")
+	gomniauth.WithProviders(
+		google.New(
+			"",
+			"",
+			"",
+		),
+	)
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
